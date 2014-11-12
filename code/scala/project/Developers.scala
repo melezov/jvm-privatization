88ffe537f564b731e58b989b626ee4d5cc51dd3f
rinmalavi@gmail.com <rinmalavi@gmail.com>
2014-11-12 17:30:40 +0100

    Publishing Settings.

---
+++ code/scala/project/Developers.scala
@@ -0,0 +1,17 @@
+object Developers {
+  lazy val members = Map(
+    "melezov" -> "Marko Elezović",
+    "rinmalavi" -> "Marin Vila"
+  )
+
+  def toXml =
+    <developers>
+      {members map { case (nick, name) =>
+        <developer>
+          <id>{ nick }</id>
+          <name>{ name }</name>
+          <url>https://github.com/{ nick }</url>
+        </developer>
+      }}
+    </developers>
+}
